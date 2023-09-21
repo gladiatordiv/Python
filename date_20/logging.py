fd4777690135ee8e90d2e40478ae5ab6e15f5543
bfgbuf <divyabwr30702@gmail.com>
2023-09-21 14:19:34 +0530

    21-09-23

---
+++ date_20/logging.py
@@ -0,0 +1,12 @@
+# import logging
+# logging.basicConfig(filename="divya.txt",
+#                     filemode='a',
+#                     format='%Y-%m-%d %H:%M:%S',)
+# for i in range(0,15):
+#     if(i%2==0):
+#         logging.warning('log warning message')
+#     elif(i%3==0):
+
+#         logging.critical('critical')
+#     else:
+#         logging.error('error')
